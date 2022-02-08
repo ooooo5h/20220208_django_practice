5b299432f28b5224d66e9beb07b189d5a9b8d391
전은형 <ooooo5h91@gmail.com>
2022-02-08 13:07:27 +0900

    모델 클래스 작성의 기본작업이 끝났다
    python manage.py inspectdb 라는 명령어를 입력하면, 스캔을 해서 클래스를 리턴해줌.. 오마이갓 세상에

--- phonebook/models.py
+++ phonebook/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Contacts(models.Model):
+    user = models.ForeignKey('Users', models.DO_NOTHING, blank=True, null=True)
+    name = models.CharField(max_length=50)
+    phone_num = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    memo = models.TextField()
+    created_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'contacts'
+
+
+class Users(models.Model):
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'users'
