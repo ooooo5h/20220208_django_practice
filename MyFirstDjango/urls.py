2e78579e3d52fc146262f27022371b9074ee96b1
전은형 <ooooo5h91@gmail.com>
2022-02-08 12:39:55 +0900

    기본적인 http 응답 내려줌
    views.py내부에서 함수를 만들었고, 그 함수의 재료로 request를 넣어야하는데 그 이유는 뭘까

--- MyFirstDjango/urls.py
+++ MyFirstDjango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from phonebook.views import test_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', test_home, name='home'),
 ]
--- phonebook/views.py
+++ phonebook/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def test_home(request): 
+    content = '<h1>홈 화면입니다.</h1>'  # HTML태그를 테스트삼아 넣어보자
+    return HttpResponse(content)
