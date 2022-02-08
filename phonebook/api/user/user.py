b3b19e19f13bd33e4a0035fb008fb967e3dd3c2d
전은형 <ooooo5h91@gmail.com>
2022-02-08 15:33:22 +0900

    필터 + 파라미터 기본 조합

--- phonebook/api/user/user.py
+++ phonebook/api/user/user.py
@@ -6,13 +6,12 @@ from phonebook.models import Users
 
 class User(View):
     
-    # get메쏘드로 접근 => 사용자 목록보기(임시)
+    # get메쏘드로 접근 => 특정 사용자 조회(임시)
     def get(self, request):
         
-        from_db_user = Users.objects.filter(email='abcabc9@naver.com').first()
-        
-        # users = [user.get_data_object() for user in from_db_users]
-        
+        # 파라미터의 email항목을 뽑아서 검색에 활용
+        from_db_user = Users.objects.filter(email=request.GET['email']).first()
+                
         return JsonResponse( {
             'code' : 200,
             'message' : '임시 - GET 테스트',
