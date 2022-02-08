14c91959808b16b9335ad1967801e610f043650f
전은형 <ooooo5h91@gmail.com>
2022-02-08 14:56:05 +0900

    ORM의 필터 기능을 활용해보기
    first()는 1개만 !
    all()은 여러개라서 []안에 담아줘야함

--- phonebook/api/user/user.py
+++ phonebook/api/user/user.py
@@ -2,14 +2,21 @@
 
 from django.http import JsonResponse
 from django.views import View
+from phonebook.models import Users
 
 class User(View):
     
-    # get메쏘드로 접근 시 처리
+    # get메쏘드로 접근 => 사용자 목록보기(임시)
     def get(self, request):
+        
+        from_db_user = Users.objects.filter(email='abcabc9@naver.com').first()
+        
+        # users = [user.get_data_object() for user in from_db_users]
+        
         return JsonResponse( {
             'code' : 200,
             'message' : '임시 - GET 테스트',
+            'user' : from_db_user.get_data_object(),
         } )
         
         
