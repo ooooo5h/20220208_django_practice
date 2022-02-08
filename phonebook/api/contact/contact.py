c133764a5377cd048398f0a4a2c71b9750037f77
전은형 <ooooo5h91@gmail.com>
2022-02-08 15:10:51 +0900

    ORM 필터 연습하기
    flask와 다르게 filter안에 그냥 value값으로 처리

--- phonebook/api/contact/contact.py
+++ phonebook/api/contact/contact.py
@@ -6,9 +6,14 @@ class Contact(View):
     
     def get(self, request):
         
+        from_db_contact = Contacts.objects.filter(name='전은형').first()
+        
         return JsonResponse({
             'code' : 200,
             'message' : '연락처 - GET 테스트',    
+            'data' : {
+                'contact' : from_db_contact.get_data_object(),
+            }
         })
         
     
